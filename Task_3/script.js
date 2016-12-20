ea399a242557ca72fd3b8740f6e45289195a0d7c
alexminich <alexminich95@gmail.com>
2016-12-20 17:00:00 +0300

    added attr function

--- Task_3/script.js
+++ Task_3/script.js
@@ -1,6 +1,9 @@
 var start = document.getElementsByClassName('start');
 var content = ' <b> ||||Added text, wow! </b> ';
 var clName = 'bigFont';
+var attrName = 'title';
+var value = 'changed attribute';
+
 
 function myAppend(content) {
     for (var i = 0; i < start.length; i++) {
@@ -23,5 +26,17 @@ function myHtml(content) {
         for (var i = 0; i < start.length; i++) {
             start[i].replaceWith(start[i].outerHTML);
         }
+    }
+}
+
+function myAttr(attrName, value) {
+    if (value !== undefined) {
+      for (var i = 0; i < start.length; i++) {
+        start[i].setAttribute(attrName, 'changed attribute | ');
+      }
+    } else {
+      for (var i = 0; i < start.length; i++) {
+          start[i].append(' ' + start[i].getAttribute(attrName));
+      }
     }
 }
