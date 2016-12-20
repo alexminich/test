92b8e50518f3ea60b4bfc4d977248b55d05cbb62
alexminich <alexminich95@gmail.com>
2016-12-20 15:07:45 +0300

    added addClass function

--- Task_3/script.js
+++ Task_3/script.js
@@ -1,8 +1,15 @@
 var start = document.getElementsByClassName('start');
 var content = '  ||||Added text, wow! ';
+var clName = 'bigFont';
 
 function myAppend(content) {
     for (var i = 0; i < start.length; i++) {
         start[i].append(content);
     }
+}
+
+function myAddClass(clName) {
+  for (var i = 0; i < start.length; i++) {
+      start[i].classList.add(clName);
+  }
 }
