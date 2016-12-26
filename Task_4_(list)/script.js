fec6041e460617ee35f8448dbe19e5dfd901da7f
alexminich <alexminich95@gmail.com>
2016-12-26 18:42:01 +0300

    setting sizes

--- Task_4_(list)/script.js
+++ Task_4_(list)/script.js
@@ -5,12 +5,12 @@ var figures = new List();
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-function Square(color, side) {
+function Square(color, size) {
     this.color = color;
-    this.side = side;
+    this.size = size;
 }
 Square.prototype.toString = function() {
-    return ('square: ' + this.color + this.side);
+    return ('square: ' + this.color + this.size);
 }
 
 function Circle(color, size) {
@@ -18,35 +18,37 @@ function Circle(color, size) {
     this.size = size;
 }
 Circle.prototype.toString = function() {
-    return ('circle: ' + this.color + this.side);
+    return ('circle: ' + this.color + this.size);
 }
 
 function addSquare() {
     var color = prompt('Color?', 'blue');
-    //  var side = prompt('Size?', 10);
-    var a = new Square(color, 150); //  var a = new Square(color, side);
+    var size = prompt('Size?', 100);
+    var a = new Square(color, +size);
     figures.add(a);
     drawList();
 }
 
 function addCircle() {
     var color = prompt('Color?', 'blue');
-    var a = new Circle(color, 150);
+    var size = prompt('Size?', 100);
+    var a = new Circle(color, +size);
     figures.add(a);
     drawList();
 }
 
 function deleteSquare() {
     var color = prompt('Color?', 'blue');
-    //  var side = prompt('Size?', 10);
-    var a = new Square(color, 150); //  var a = new Square(color, side);
+    var size = prompt('Size?', 100);
+    var a = new Square(color, +size);
     figures.delete(a);
     drawList();
 }
 
 function deleteCircle() {
     var color = prompt('Color?', 'blue');
-    var a = new Circle(color, 150);
+    var size = prompt('Size?', 100);
+    var a = new Circle(color, +size);
     figures.delete(a);
     drawList();
 }
@@ -56,28 +58,37 @@ function drawList() {
     ctx.clearRect(0, 0, 1400, 3000);
     let x = 1;
     let y = 1;
+    let yMax = 1;
+    let line = 1;
     let elem = figures.start;
     while (elem !== null) {
+        if (elem.data.size > 1350 - x) { // переход на новую строчку
+            line = yMax + line;
+            y = line;
+            yMax = 1;
+            x = 1;
+        }
         if (elem.data instanceof Circle) {
-            var gradient = ctx.createRadialGradient(x + 75, y + 75, 75, x + 75, y + 75, 0);
+            var radius = elem.data.size / 2;
+            var gradient = ctx.createRadialGradient(x + radius, y + radius, radius, x + radius, y + radius, 0);
             gradient.addColorStop(0, '#F5FFFA');
             gradient.addColorStop(1, elem.data.color);
             gradient.addColorStop(0.7, elem.data.color);
         } else {
-            var gradient = ctx.createLinearGradient(x, 0, x + 150, 0);
+            var gradient = ctx.createLinearGradient(x, 0, x + elem.data.size, 0);
             gradient.addColorStop(0, '#F5FFFA');
             gradient.addColorStop(0.4, elem.data.color);
             gradient.addColorStop(0.6, elem.data.color);
             gradient.addColorStop(1, '#F5FFFA');
         }
         ctx.fillStyle = gradient;
-        ctx.fillRect(x, y, 150, 150);
+        ctx.fillRect(x, y, elem.data.size, elem.data.size);
+        ctx.strokeText(String(elem.data.size), x + elem.data.size / 2 - 10, y + elem.data.size / 2 + 2);
 
-        x += 153;
-        if (x > 1200) {
-            x = 1;
-            y += 153;
+        if (elem.data.size + line > yMax) {
+            yMax = elem.data.size + line;
         }
+        x += elem.data.size;
         elem = elem.next;
     }
 }
