d0eb0f1e9ffbf91cf1f2c3318ddb35d5fccf39df
alexminich <alexminich95@gmail.com>
2016-12-30 17:17:45 +0300

    search recieves answer

--- Task_5/script.js
+++ Task_5/script.js
@@ -2,13 +2,20 @@
 
 
 
-function request() {
+function doSearch () {
+  var form = document.forms.search;
+  var searchRequest = form.elements.searchRequest.value;
+  request(searchRequest);
+}
+
+
+function request(request) {
     var xhr = new XMLHttpRequest();
 
     var params = 'part=' + encodeURIComponent('snippet') +
         '&key=' + encodeURIComponent("AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ") +
         '&maxResults=' + encodeURIComponent(2) +
-        '&q=' + encodeURIComponent('cat') +
+        '&q=' + encodeURIComponent(request) +
         '&type=' + encodeURIComponent('video');
 
     xhr.open('GET', 'https://www.googleapis.com/youtube/v3/search?' + params, true);
