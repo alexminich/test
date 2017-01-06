55b24d0e16d886ed4aa58f1b9f389799792975bc
alexminich <alexminich95@gmail.com>
2017-01-06 15:51:29 +0300

    added statistics

--- Task_5/script.js
+++ Task_5/script.js
@@ -6,38 +6,44 @@ var request = "";
 var itemsCount = 0;
 var list;
 
+
 function doSearch() {
     nextPageToken = 0;
     var form = document.forms.search;
     var searchRequest = form.elements.searchRequest.value;
     request = searchRequest;
     doRequest(request);
-    //  createList(doRequest(searchRequest));
-
-    // console.log(a);
-    // var a = doRequest(searchRequest);
-    // console.log(a);
-    // createList(a);
 }
 
 
 function doRequest(request) {
     var xhr = new XMLHttpRequest();
 
-
-    var params = 'part=' + 'snippet' +
-        '&key=' + "AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ" +
-        '&maxResults=' + 15 +
-        '&q=' + request +
-        '&type=' + 'video';
+    if (nextPageToken === 0) {
+        var params = 'part=' + 'snippet' +
+            '&key=' + "AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ" +
+            '&maxResults=' + 15 +
+            '&q=' + request +
+            '&type=' + 'video';
+    } else {
+        var params = 'part=' + 'snippet' +
+            '&key=' + "AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ" +
+            '&maxResults=' + 15 +
+            '&q=' + request +
+            '&type=' + 'video' +
+            '&pageToken=' + nextPageToken;
+    }
 
     xhr.open('GET', 'https://www.googleapis.com/youtube/v3/search?' + params, true);
 
-    xhr.send();
-
     xhr.onload = function() {
-        createList(this.responseText);
-        // return this.responseText;
+        if (nextPageToken === 0) {
+            createList(this.responseText);
+        } else {
+            var response = JSON.parse(this.responseText);
+            nextPageToken = response.nextPageToken;
+            appendItems(response);
+        }
     }
 
     xhr.onerror = function() {
@@ -45,41 +51,16 @@ function doRequest(request) {
         return 0;
     }
 
-    // xhr.onreadystatechange = function() {
-    //   if (this.readyState != 4) return;
-    //
-    //   if (this.status != 200) {
-    //     // обработать ошибку
-    //     alert( 'ошибка: ' + (this.status ? this.statusText : 'запрос не удался') );
-    //     return;
-    //   }
-    //   var a = this.responseText;
-    //   return a;
-
-    // var timerId = setInterval(function() {
-    //   xhr.onload = function() {
-    //       var a = this.responseText;
-    //       return a;
-    //       clearInterval(timerId);
-    //     }
-    //
-    //     xhr.onerror = function() {
-    //       alert('ошибка ' + this.status);
-    //       return 0;
-    //     }
-    // }, 200);
-
-
-    // }
+    xhr.send();
 }
 
 
-function createList(resp) {
+function createList(serverResponse) {
     if (document.body.contains(document.getElementsByClassName('wrap')[0])) {
         document.body.removeChild(document.getElementsByClassName('wrap')[0]);
     }
 
-    var response = JSON.parse(resp);
+    var response = JSON.parse(serverResponse);
     console.log(response);
     nextPageToken = response.nextPageToken;
 
@@ -94,10 +75,10 @@ function createList(resp) {
     leftButton.className = "leftButton";
     rightButton.className = "rightButton";
     leftButton.onclick = function() {
-        scrollLeft(list)
+        scrollLeft(list);
     };
     rightButton.onclick = function() {
-        scrollRight(list)
+        scrollRight(list);
     };
 
     appendItems(response);
@@ -127,7 +108,7 @@ function scrollRight(list) {
 }
 
 
-function resize(wrap, leftButton, rightButton, previousWidth) {
+function resize(wrap, leftButton, rightButton) {
     wrap.style.width = document.documentElement.clientWidth + 'px';
     let elementWidth = document.getElementsByClassName('resultElem')[0].offsetWidth + 10;
     let countVisibleElements = Math.floor((wrap.clientWidth - leftButton.offsetWidth - rightButton.offsetWidth) / elementWidth);
@@ -136,38 +117,8 @@ function resize(wrap, leftButton, rightButton, previousWidth) {
     wrap.style.width = wrapWidth + 'px';
 }
 
-// подгрузка элементов
-function loadItems() {
-    if (nextPageToken === 0) {
-        return
-    };
 
-    var xhr = new XMLHttpRequest();
-
-    var params = 'part=' + 'snippet' +
-        '&key=' + "AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ" +
-        '&maxResults=' + 15 +
-        '&q=' + request +
-        '&type=' + 'video' +
-        '&pageToken=' + nextPageToken;
-
-    xhr.open('GET', 'https://www.googleapis.com/youtube/v3/search?' + params, true);
-
-    xhr.send();
-
-    xhr.onload = function() {
-        var response = JSON.parse(this.responseText);
-        nextPageToken = response.nextPageToken;
-        appendItems(response);
-        // return this.responseText;
-    }
-
-    xhr.onerror = function() {
-        alert('ошибка ' + this.status);
-        return 0;
-    }
-}
-
+// добавляем видео в результаты поиска
 function appendItems(response) {
     for (var i = 0; i < 15; i++) {
         let elem = document.createElement('div'); // element block creation
@@ -190,26 +141,68 @@ function appendItems(response) {
         let linkBlock = document.createElement('div'); // link Block creation
         linkBlock.className = "linkBlock";
 
+        let channel = document.createElement('p'); // cahnnel title creation
+        channel.innerHTML = 'Channel: <b>' + '<a href="https://www.youtube.com/channel/' + response.items[i].snippet.channelId + '" target=_blank>' + response.items[i].snippet.channelTitle + '</a></b><br><br>';
+
         let description = document.createElement('div'); // description and date creation
         var date = new Date(response.items[i].snippet.publishedAt);
-        var options = {
+        description.className = "description";
+        description.innerHTML = response.items[i].snippet.description + '<br><br>' + '<b>опубликовано:</b> ' + date.toLocaleString("ru", {
             year: 'numeric',
             month: 'long',
             day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric'
-        }
-        description.className = "description";
-        description.innerHTML = response.items[i].snippet.description + '<br><br>' + '<b>опубликовано:</b> ' + date.toLocaleString("ru", options);
+        }) + '<br><br>';
+
+        getStatistics(response.items[i].id.videoId, elem);
 
         linkBlock.appendChild(thumbnail);
         linkBlock.appendChild(title);
         link.appendChild(linkBlock);
 
         elem.appendChild(link);
+        elem.appendChild(channel);
         elem.appendChild(description);
+
         list.appendChild(elem);
         itemsCount++;
     }
+}
+
+
+// получение количества просмотров и лайков
+function getStatistics(videoId, elem) {
+    var xhr = new XMLHttpRequest();
+
+    var params = 'part=' + 'statistics' +
+        '&key=' + "AIzaSyAqDkjeDHD6SK9PN-eun3NZR0Fzws8qgAQ" +
+        '&id=' + videoId;
+
+    xhr.open('GET', 'https://www.googleapis.com/youtube/v3/videos?' + params, true);
+
+    xhr.onload = function() {
+        var response = JSON.parse(this.responseText);
+        let statistics = document.createElement('div');
+        statistics.className = "statistics";
+        let watchesIcon = document.createElement('i');
+        watchesIcon.className = "fa fa-eye";
+        let likeIcon = document.createElement('i');
+        likeIcon.className = "fa fa-thumbs-up";
+        let dislikeIcon = document.createElement('i');
+        dislikeIcon.className = "fa fa-thumbs-down";
+        statistics.appendChild(watchesIcon);
+        var formatter = new Intl.NumberFormat("ru");
+        statistics.innerHTML += ' : <b>' + formatter.format(response.items[0].statistics.viewCount) + '</b><br>';
+        statistics.appendChild(likeIcon);
+        statistics.innerHTML += formatter.format(response.items[0].statistics.likeCount) + ' ';
+        statistics.appendChild(dislikeIcon);
+        statistics.innerHTML += formatter.format(response.items[0].statistics.dislikeCount);
+        elem.appendChild(statistics);
+    }
+
+    xhr.onerror = function() {
+        alert('ошибка ' + this.status);
+        return 0;
+    }
+
+    xhr.send();
 }
